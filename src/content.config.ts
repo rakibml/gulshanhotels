205a1c107c775686e0742f6faccbe8f489b9bf0e
rakibml <129305610+rakibml@users.noreply.github.com>
2026-08-02 01:07:51 +0600

    Update content.config.ts

--- src/content.config.ts
+++ src/content.config.ts
@@ -1,18 +1,14 @@
-// src/content.config.ts
 import { defineCollection, z } from 'astro:content';
-import { glob } from 'astro/loaders'; // Astro v5 Loader
+import { glob } from 'astro/loaders'; // Astro Content Layer Loader
 
 const hotels = defineCollection({
-  loader: glob({ pattern: '**/[^_]*.{md,mdx}', base: "./src/content/hotels" }),
+  loader: glob({ pattern: '**/[^_]*.{md,mdx}', base: './src/content/hotels' }),
   schema: z.record(z.any()),
 });
 
 const sponsored = defineCollection({
-  loader: glob({ pattern: '**/[^_]*.{md,mdx}', base: "./src/content/sponsored" }),
+  loader: glob({ pattern: '**/[^_]*.{md,mdx}', base: './src/content/sponsored' }),
   schema: z.record(z.any()),
 });
 
-export const collections = {
-  hotels,
-  sponsored,
-};
+export const collections = { hotels, sponsored };
