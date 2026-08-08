ede01e8bdc5b48b9da98015dea276c3677bd4e2b
rakibml <129305610+rakibml@users.noreply.github.com>
2026-08-08 23:15:07 +0600

    Remove loader from hotels collection configuration

--- src/content/content.config.ts
+++ src/content/content.config.ts
@@ -1,8 +1,6 @@
 import { defineCollection, z } from 'astro:content';
-import { glob } from 'astro/loaders';
 
 const hotelsCollection = defineCollection({
-  loader: glob({ pattern: '**/*.md', base: './src/content/hotels' }),
   schema: z.object({}).passthrough(),
 });
 
