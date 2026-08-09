e7057365fd4b7b097e90c0eca696d35c7e65a43c
rakibml <129305610+rakibml@users.noreply.github.com>
2026-08-09 21:00:20 +0600

    Update content.config.ts

--- src/content/content.config.ts
+++ src/content/content.config.ts
@@ -1,10 +1,11 @@
 import { defineCollection, z } from 'astro:content';
+import { glob } from 'astro/loaders';
 
-const hotelsCollection = defineCollection({
-  type: 'content',
+const hotels = defineCollection({
+  loader: glob({ pattern: '**/*.md', base: './src/content/hotels' }),
   schema: z.object({}).passthrough(),
 });
 
 export const collections = {
-  hotels: hotelsCollection,
+  hotels,
 };
