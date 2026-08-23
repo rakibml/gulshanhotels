48ae2f0c328c823f05d925c8a4389838958eae12
rakibml <129305610+rakibml@users.noreply.github.com>
2026-08-23 18:40:06 +0600

    Remove Tailwind integration from astro.config.mjs
    Removed Tailwind integration from Astro config.

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,9 @@
 // astro.config.mjs
 import { defineConfig } from 'astro/config';
-import tailwind from '@astrojs/tailwind';
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind()],
   build: {
-    inlineStylesheets: 'always', // <--- এই সেটিংটি রেন্ডার-ব্লকিং সিএসএস বন্ধ করে ১৪০ms স্পিড বাড়িয়ে দেবে
+    inlineStylesheets: 'always', // Inlines stylesheets to eliminate render-blocking CSS
   },
 });
