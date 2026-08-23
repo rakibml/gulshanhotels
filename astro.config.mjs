7a5de8298ea54a7ae26f02e2ff79f9339a7f1b43
rakibml <129305610+rakibml@users.noreply.github.com>
2026-08-23 18:42:18 +0600

    Update astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,8 +2,4 @@
 import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
-export default defineConfig({
-  build: {
-    inlineStylesheets: 'always', // Inlines stylesheets to eliminate render-blocking CSS
-  },
-});
+export default defineConfig({});
